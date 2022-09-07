99b2cae2ff8200bab01d8aab969433b6d0b1801c
JoseXS <XSkunk@gmail.com>
2022-09-07 11:30:27 +0200

    feat: Se completa contacto y demas mejoras

--- articles.js
+++ articles.js
@@ -2,20 +2,21 @@ const responseDev = httpGet('https://dev.to/api/articles?username=josexs')
 const devArticles = JSON.parse(responseDev);
 
 const articlesData = [];
-console.log(devArticles.length)
 devArticles.forEach((article) => {
     const data = {
         title: article.title,
         tags: article.tag_list,
         url: article.url,
         description: article.description,
-        image: article.cover_image,
+        image: article.social_image,
         created: article.published_at,
         readTimeMinutes: article.readable_publish_date,
         reactions: article.public_reactions_count
     }
     return articlesData.push(data)
 })
+
+console.group(devArticles);
 
 const createTagsForArticles = () => {
   const tags = [];
@@ -28,30 +29,65 @@ const createTagsForArticles = () => {
   });
 
   const filterContainer = document.getElementById("filterArticles");
-  filterContainer.innerHTML += `<button class="btn filter-btn active">Todos</button>`;
+  filterContainer.innerHTML += `<button class="btn filter-btn filter-article active">Todos</button>`;
 
   tags.forEach((tag) => {
-    filterContainer.innerHTML += `<button class="btn filter-btn capitalize">${tag}</button>`;
+    filterContainer.innerHTML += `<button class="btn filter-btn filter-article capitalize">${tag}</button>`;
   });
 };
 
 const createArticlesCards = (data) => {
   let articleContainer = document.getElementById("article-container");
   articleContainer.innerHTML += `
-            <div class="article-card" data-tags="${data.tags}">
-                <div class="article-wrapper">
-
-                    <p class="article-title">⭐️ ${data.title}</p>
-
-                    <div class="project-body">
-                        <h1 class="project-name">${data.title}</h1>
-                        <p class="project-detail">${data.description}</p>
-                        <a href="${data.url}" target="_blank" class="btn">Ver en Dev.to</a>
-                    </div>
-                </div>
-            </div>
-    `;
+    <div class="article-card" data-tags="${data.tags}" data-url="${data.url}">
+      <div class="article-wrapper">
+        <div class="article-thumbnail">
+            <img src="assets/img/close.png" class="close-btn" alt="">
+            <img src="${data.image}" class="article-image" alt="${data.name}">
+        </div>
+        <div class="article-body">
+          <p class="article-title">${data.title}</p>
+          <p class="article-detail">${data.description}</p>
+          <p class="article-tags">${data.tags}</p>
+        </div>
+      </div>
+    </div>
+  `;
 };
 
 createTagsForArticles();
 articlesData.forEach((data) => createArticlesCards(data));
+
+// article cards open link
+const articles = document.querySelectorAll(".article-card");
+console.log({articles})
+
+articles.forEach((card) => {
+  card.addEventListener("click", (e) => {
+    const url = card.getAttribute("data-url");
+    window.open(url, '_blank');
+  });
+});
+
+
+// article filter function
+const tagsArticles = document.querySelectorAll(".filter-article");
+tagsArticles.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    articles.forEach((card) => {
+      if (btn.innerHTML.toLowerCase() == "todos") {
+        card.style.display = "block";
+      } else {
+        console.log(card.getAttribute("data-tags"));
+        if (card.getAttribute("data-tags").includes(btn.innerHTML.toLowerCase())) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+      }
+    });
+
+    tagsArticles.forEach((item) => item.classList.remove("active"));
+    btn.classList.add("active");
+  });
+});
--- project.js
+++ project.js
@@ -28,11 +28,10 @@ const createTagsForProjects = () => {
   });
 
   const filterContainer = document.getElementById("filterProjects");
-  console.log(filterContainer)
-  filterContainer.innerHTML += `<button class="btn filter-btn active">Todos</button>`;
+  filterContainer.innerHTML += `<button class="btn filter-btn filter-project active">Todos</button>`;
 
   tags.forEach((tag) => {
-    filterContainer.innerHTML += `<button class="btn filter-btn capitalize">${tag}</button>`;
+    filterContainer.innerHTML += `<button class="btn filter-btn filter-project capitalize">${tag}</button>`;
   });
 };
 
@@ -40,7 +39,7 @@ const createProjectCards = (data) => {
   let projectContainer = document.getElementById("project-container");
 
   projectContainer.innerHTML += `
-            <div class="project-card" data-tags="${data.tags}">
+            <div class="project-card" project-tags="${data.tags}">
                 <div class="project-wrapper">
                     <div class="project-thumbnail">
                         <img src="assets/img/close.png" class="close-btn" alt="">
@@ -60,3 +59,49 @@ const createProjectCards = (data) => {
 
 createTagsForProjects();
 projectData.forEach((data) => createProjectCards(data));
+
+
+// project cards open and close functions
+const projects = document.querySelectorAll(".project-card");
+
+projects.forEach((card, index) => {
+  let closeBtn = card.querySelector(".close-btn");
+  closeBtn.addEventListener("click", () => {
+    projects.forEach((item, i) => {
+      item.classList.remove("blur");
+    });
+    card.classList.remove("active");
+  });
+
+  card.addEventListener("click", (e) => {
+    if (e.path[0] != closeBtn) {
+      projects.forEach((item, i) => {
+        if (i != index) {
+          item.classList.add("blur");
+        }
+      });
+      card.classList.add("active");
+    }
+  });
+});
+
+// project filter function
+const tagsProjects = document.querySelectorAll(".filter-project");
+tagsProjects.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    projects.forEach((card) => {
+      if (btn.innerHTML.toLowerCase() == "todos") {
+        card.style.display = "block";
+      } else {
+        if (card.getAttribute("project-tags").includes(btn.innerHTML.toLowerCase())) {
+          card.style.display = "block";
+        } else {
+          card.style.display = "none";
+        }
+      }
+    });
+
+    tagsProjects.forEach((item) => item.classList.remove("active"));
+    btn.classList.add("active");
+  });
+});
